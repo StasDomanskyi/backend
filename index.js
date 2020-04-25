e4e367d69b5390aff9f64ffff3f40827c441cab7
StXt <stas.domanskiy@gmail.com>
2020-04-25 22:56:19 +0300

    added more req

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const r1 = require('./payload/r1.js');
 const port = 3000;
+const r1 = require('./payload/r1.js');
+const r2 = require('./payload/r2.js');
+const r3 = require('./payload/r3.js');
+const r4 = require('./payload/r4.js');
+
 
 
 var corsOptions = {
@@ -21,11 +25,21 @@ app.get('/', (req, res) => {
     text: "Hello World!"
   });
 });
+
 // R1 - TABLE
 app.get('/r1', r1.getAll);
-app.get('/r1/:key/:value', r1.getClient);
+app.get('/r1/:key/:status/:value', r1.getClient);
 app.post('/r1', jsonParser, r1.postClient);
 app.put('/r1/:key/:value', jsonParser, r1.updateClient);
 app.delete('/r1/:key/:value', r1.deleteClient);
+
+// R2 - TABLE
+app.get('/r2/:status/:account', r2.getAccount);
+
+// R3 - TABLE
+app.get('/r3/:key/:value/:domain', r3.getService);
+
+// R4 - TABLE
+app.get('/r4/:key/:value/:status', r4.getService);
 
 app.listen( port, () => console.log(`"Hosting Company" listening at http://localhost:${port}`));
--- payload/r2.js
+++ payload/r2.js
@@ -1,40 +1,41 @@
 const connection = require('./database');
 
 module.exports = {
-  getAll: (req, res) => {
-    connection.query('SELECT * FROM r1', (err, data) => {
+/*   getAll: (req, res) => {
+    connection.query('SELECT * FROM r2', (err, data) => {
       res.send(data);
     })
-  },
+  }, */
 
-  getClient: (req, res) => {
-    let query = req.params.key === 'user_name' ?
-    `SELECT * FROM r1 WHERE ${req.params.key} LIKE "%${req.params.value}%" AND user_status = ${req.params.status}` :
-    `SELECT * FROM r1 WHERE ${req.params.key} = ${req.params.value} AND user_status = ${req.params.status}`;
+  getAccount: (req, res) => {
+    let query = `SELECT * FROM r2 WHERE account_status = ${req.params.status}
+    AND account_description LIKE "%${req.params.account}%"`;
+    
+    console.log(query);
             
-    connection.query(query, (err, data) => {
+    connection.query(query, (err, data) => {      
       res.send(data);
     });
   },
 
-  postClient: (req, res) => { 
+  /* postClient: (req, res) => { 
     console.log(req.body); 
     connection.query(`
       INSERT r1(user_number, user_name, email, phone, user_status, balance)
       VALUES ('${req.body.user_number}', '${req.body.user_name}', '${req.body.email}', '${req.body.phone}', '${req.body.status}', '${req.body.balance}')`, (err, data) => { 
       res.send(req.body);
     });
-  },
+  }, */
 
-  updateClient: (req, res) => {
+  /* updateClient: (req, res) => {
     connection.query(`UPDATE r1
     SET ${req.body.field} = '${req.body.valueOfField}'
     WHERE ${req.body.key} = ${req.body.valueOfKey};`, (err, data) => {
       res.send(req.body);
     });
-  },
+  }, */
 
-  deleteClient: (req, res) => {
+  /* deleteClient: (req, res) => {
     let query = req.params.key === 'user_name' ?
     `DELETE FROM r1 WHERE ${req.params.key} = "${req.params.value}"` :
     `DELETE FROM r1 WHERE ${req.params.key} = ${req.params.value}`;
@@ -42,5 +43,5 @@ module.exports = {
     connection.query(query, (err, data) => {
       res.send({message: 'deleted'});
     });
-  }
+  } */
 }
